8726c2f566f5b23c9b396a45353ddbb8e5461f8b
rpcx <rpcxdev@gmail.com>
2022-09-06 15:12:05 +0700

    add examples components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 // import '../css/app.css'
 import '../css/tailwind.css'
+// import 'chart.js'
+import Chart from 'chart.js/auto';
 
 import Alpine from 'alpinejs'
 
--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
+Route.get('/test', async ({ view }) => {
+  return view.render('test')
+})
